d016572a3f054fdb68fdd652bee2f1918861eacc
jpchung <jpchungjpc@gmail.com>
2017-03-01 13:35:36 -0700

    Created Page Switcher Class, MainWindow opens on Login Page by default. Login Page Horizontal Prototype

---
+++ CPSC_481_PROJECT/CPSC_481_PROJECT/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CPSC_481_PROJECT
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        /// <summary>
+        /// Initialize WPF MainWindow components on start
+        /// </summary>
+        public MainWindow()
+        {
+            InitializeComponent();
+            //initialize MainWindow to Login page by default
+            PageSwitcher.pageSwitchWindow = this;
+            PageSwitcher.Switch(new LoginPage());
+        }
+
+        /// <summary>
+        /// Navigate to UserControl page and display contents in MainWindow
+        /// </summary>
+        /// <param name="nextPage"></param>
+        public void Navigate(UserControl nextPage)
+        {
+            this.Content = nextPage;
+        }
+    }
+}
---
+++ CPSC_481_PROJECT/CPSC_481_PROJECT/PageSwitcher.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace CPSC_481_PROJECT
+{
+    class PageSwitcher
+    {
+        //instantiation of MainWindow to switch UserControl pages on
+        public static MainWindow pageSwitchWindow;
+        
+        /// <summary>
+        /// Switch which UserControl Page for MainWindow to display
+        /// </summary>
+        /// <param name="newPage"></param>
+        public static void Switch(UserControl newPage)
+        {
+            pageSwitchWindow.Navigate(newPage);
+        }
+
+    }
+}
