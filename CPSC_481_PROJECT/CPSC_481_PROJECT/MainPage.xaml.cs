799da6b00cb7b0356b2be0b20dbc858a84bd7d93
jpchung <jpchungjpc@gmail.com>
2017-03-03 15:08:23 -0700

    added Listview and SearchBar with glossary

--- CPSC_481_PROJECT/CPSC_481_PROJECT/MainPage.xaml.cs
+++ CPSC_481_PROJECT/CPSC_481_PROJECT/MainPage.xaml.cs
@@ -56,7 +56,7 @@ namespace CPSC_481_PROJECT
         private void ProfileImage_Initialized(object sender, EventArgs e)
         {
             //ProfileImage.Source = new BitmapImage(new Uri("http://68.media.tumblr.com/78f1e0e0197fc6f0f4f839d0214a7b47/tumblr_o7ww2x2z2A1rsd6nxo4_250.png"));
-            ProfileImage.Source = new BitmapImage(new Uri("pack://application:,,,/JUSTICE.png"));
+            ProfileImage.Source = new BitmapImage(new Uri("pack://application:,,,/Images/JUSTICE.png"));
         }
     }
 }
